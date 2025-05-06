06105abdfc3a93ba42771e9548f7f17ae658c1b8
Gabriel Puente <gabrielbuzz@BUZZFI0001.>
2025-05-06 15:19:46 -0600

    Navbar/hero is done

---
+++ client/js/index.js
@@ -0,0 +1,5 @@
+document.getElementById('hamburger').addEventListener('click', function () {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+  });
+  
